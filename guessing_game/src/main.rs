658071d754327cf745004a48c0b2808cc039b409
heguangfu <hgfkeep@gmail.com>
2021-02-22 11:19:14 +0800

    get user input  number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,10 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Gussing the number!");
+    println!("Input your number:");
+
+    let mut gussing = String::new();
+    io::stdin().read_line(&mut gussing).expect("failed to read line!");
+
+    println!("you guessed: {}", gussing);
 }
